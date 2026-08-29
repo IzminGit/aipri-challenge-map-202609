0c36e0be1055463366bbb4c176b606edf749e6b7
いずみん <35645414+IzminGit@users.noreply.github.com>
2026-08-29 22:18:19 +0900

    Add initial 2026-09 event data

---
+++ data.js
@@ -0,0 +1,9 @@
+window.AIPRI_EVENT_DATA = {
+  eventName: 'お店でアイプリチャレンジ！メルヘンフェスコーデをゲット',
+  eventId: '10',
+  challengeId: 'aipri-challenge-202609',
+  prefecture: '全国',
+  sourceUrl: 'https://aipri.jp/event/e2608271/',
+  fetchedAt: null,
+  shops: []
+};
