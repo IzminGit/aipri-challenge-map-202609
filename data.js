301999012b5f1b8d2d3bf24881324ca41fc02bc5
いずみん <35645414+IzminGit@users.noreply.github.com>
2026-08-29 22:31:08 +0900

    Seed verified September challenge data for initial deployment

--- data.js
+++ data.js
@@ -4,6 +4,64 @@ window.AIPRI_EVENT_DATA = {
   challengeId: 'aipri-challenge-202609',
   prefecture: '全国',
   sourceUrl: 'https://aipri.jp/event/e2608271/',
-  fetchedAt: null,
-  shops: []
+  fetchedAt: '2026-08-29T13:30:00+09:00',
+  bootstrap: true,
+  shops: [
+    {
+      id:'bootstrap-1', name:'GiGO イオンタウン名西', address:'愛知県名古屋市西区香呑町6-49-1', prefecture:'愛知県', machineTypes:['おねがいアイプリ','アイプリバース'], participation:'参加費1名200円・定員30名を超えた場合抽選', mapsSearchUrl:'https://www.google.com/maps/search/?api=1&query='+encodeURIComponent('GiGO イオンタウン名西 愛知県名古屋市西区香呑町6-49-1'), events:[
+        {id:'bootstrap-1-event-1',label:'大会①',date:'2026-09-12',dateDisplay:'26年9月12日（土）',ageLimit:'中学生以下',startTime:'11:00',registrationTime:'9:30～10:00',lotteryTime:'10:15',note:'受け付けは、カウンター前で行います'},
+        {id:'bootstrap-1-event-2',label:'大会②',date:'2026-09-13',dateDisplay:'26年9月13日（日）',ageLimit:'年齢無制限',startTime:'11:00',registrationTime:'9:30～10:00',lotteryTime:'10:15',note:'受け付けは、カウンター前で行います'}]
+    },
+    {
+      id:'bootstrap-2', name:'namco イオンモール大高店', address:'愛知県名古屋市緑区南大高二丁目450 イオンモール大高3F', prefecture:'愛知県', machineTypes:['おねがいアイプリ','アイプリバース','アイプリステーション'], participation:'参加費1名200円・定員30名を超えた場合抽選', events:[
+        {id:'bootstrap-2-event-1',label:'大会',date:'2026-09-13',dateDisplay:'26年9月13日（日）',ageLimit:'中学生以下',startTime:'11:00',registrationTime:'10:00～11:00',lotteryTime:'11:00',note:''}]
+    },
+    {
+      id:'bootstrap-3', name:'モーリーファンタジーワンダーシティ店', address:'愛知県名古屋市西区二方町40 イオンスタイル3F', prefecture:'愛知県', machineTypes:['おねがいアイプリ','アイプリバース','アイプリステーション'], participation:'参加費1名200円・定員30名を超えた場合抽選', events:[
+        {id:'bootstrap-3-event-1',label:'大会①',date:'2026-09-26',dateDisplay:'26年9月26日（土）',ageLimit:'中学生以下',startTime:'10:00',registrationTime:'9:00～9:30',lotteryTime:'9:30',note:'抽選が終わり次第大会開始'},
+        {id:'bootstrap-3-event-2',label:'大会②',date:'2026-09-27',dateDisplay:'26年9月27日（日）',ageLimit:'年齢無制限',startTime:'10:00',registrationTime:'9:00～9:30',lotteryTime:'9:30',note:'抽選が終わり次第大会開始'}]
+    },
+    {
+      id:'bootstrap-4', name:'ｎａｍｃｏ太鼓の達人毎日おまつりだドン イオンモール長久手店', address:'愛知県長久手市勝入塚501 イオンモール長久手4F', prefecture:'愛知県', machineTypes:['おねがいアイプリ','アイプリバース'], participation:'参加費1名200円・定員30名を超えた場合抽選', events:[
+        {id:'bootstrap-4-event-1',label:'大会①',date:'2026-09-12',dateDisplay:'26年9月12日（土）',ageLimit:'中学生以下',startTime:'13:00',registrationTime:'12:00～12:20',lotteryTime:'12:30',note:''},
+        {id:'bootstrap-4-event-2',label:'大会②',date:'2026-09-22',dateDisplay:'26年9月22日（火祝）',ageLimit:'年齢無制限',startTime:'13:00',registrationTime:'12:00～12:20',lotteryTime:'12:30',note:''}]
+    },
+    {
+      id:'bootstrap-5', name:'モーリーファンタジーイオン岡崎店', address:'愛知県岡崎市戸崎町字ばら山1-1 イオン', prefecture:'愛知県', machineTypes:['おねがいアイプリ','アイプリバース'], participation:'参加費1名200円・定員30名を超えた場合抽選', events:[
+        {id:'bootstrap-5-event-1',label:'大会',date:'2026-09-12',dateDisplay:'26年9月12日（土）',ageLimit:'中学生以下',startTime:'11:00',registrationTime:'10:00～10:30',lotteryTime:'10:30',note:'抽選終わり次第大会開催'}]
+    },
+    {
+      id:'bootstrap-6', name:'モーリーファンタジー t-FACE店', address:'愛知県豊田市西町6-85-1 t-FACE A館8F', prefecture:'愛知県', machineTypes:['おねがいアイプリ','アイプリバース'], participation:'参加費1名200円・定員30名を超えた場合抽選', events:[
+        {id:'bootstrap-6-event-1',label:'大会①',date:'2026-09-13',dateDisplay:'26年9月13日（日）',ageLimit:'年齢無制限',startTime:'11:00',registrationTime:'10:00～10:30',lotteryTime:'10:30',note:'抽選が終わり次第イベント開始'},
+        {id:'bootstrap-6-event-2',label:'大会②',date:'2026-09-23',dateDisplay:'26年9月23日（水祝）',ageLimit:'中学生以下',startTime:'11:00',registrationTime:'10:00～10:30',lotteryTime:'10:30',note:'抽選が終わり次第イベント開始'}]
+    },
+    {
+      id:'bootstrap-7', name:'namco 池袋店', address:'東京都豊島区東池袋1-22-10 ヒューマックスパビリオン池袋サンシャイン60通り 1F', prefecture:'東京都', machineTypes:['おねがいアイプリ','アイプリバース'], participation:'参加費1名200円・定員30名を超えた場合抽選', events:[
+        {id:'bootstrap-7-event-1',label:'大会①',date:'2026-09-12',dateDisplay:'26年9月12日（土）',ageLimit:'中学生以下',startTime:'12:00',registrationTime:'10:00～11:00',lotteryTime:'11:30',note:''},
+        {id:'bootstrap-7-event-2',label:'大会②',date:'2026-09-13',dateDisplay:'26年9月13日（日）',ageLimit:'年齢無制限',startTime:'12:00',registrationTime:'10:00～11:00',lotteryTime:'11:30',note:''}]
+    },
+    {
+      id:'bootstrap-8', name:'モーリーファンタジー東久留米店', address:'東京都東久留米市南沢5-17 イオンモール3F', prefecture:'東京都', machineTypes:['おねがいアイプリ','アイプリバース','アイプリステーション'], participation:'参加費1名200円・定員30名を超えた場合抽選', events:[
+        {id:'bootstrap-8-event-1',label:'大会',date:'2026-09-12',dateDisplay:'26年9月12日（土）',ageLimit:'中学生以下',startTime:'10:00',registrationTime:'9:00～9:30',lotteryTime:'9:30',note:'抽選が終わり次第大会開始'}]
+    },
+    {
+      id:'bootstrap-9', name:'GiGO神楽坂', address:'東京都新宿区神楽坂2-11 第2カグラヒルズ1F', prefecture:'東京都', machineTypes:['おねがいアイプリ','アイプリバース'], participation:'参加費1名200円・定員30名を超えた場合抽選', events:[
+        {id:'bootstrap-9-event-1',label:'大会①',date:'2026-09-12',dateDisplay:'26年9月12日（土）',ageLimit:'中学生以下',startTime:'13:30',registrationTime:'10:00～12:30',lotteryTime:'13:00',note:'受付は本館で行います'},
+        {id:'bootstrap-9-event-2',label:'大会②',date:'2026-09-13',dateDisplay:'26年9月13日（日）',ageLimit:'年齢無制限',startTime:'13:30',registrationTime:'10:00～12:30',lotteryTime:'13:00',note:'受付は本館で行います'}]
+    },
+    {
+      id:'bootstrap-10', name:'namco イオンモール佐久平店', address:'長野県佐久市佐久平駅南11-10 イオンモール佐久平2F', prefecture:'長野県', machineTypes:['おねがいアイプリ','アイプリバース','アイプリステーション'], participation:'参加費1名200円・定員30名を超えた場合抽選', events:[
+        {id:'bootstrap-10-event-1',label:'大会①',date:'2026-09-12',dateDisplay:'26年9月12日（土）',ageLimit:'中学生以下',startTime:'13:30',registrationTime:'12:30～13:15',lotteryTime:'13:15',note:'定員割れの場合も競技順を決める抽選を行います。'},
+        {id:'bootstrap-10-event-2',label:'大会②',date:'2026-09-21',dateDisplay:'26年9月21日（月祝）',ageLimit:'年齢無制限',startTime:'13:30',registrationTime:'12:30～13:15',lotteryTime:'13:15',note:'定員割れの場合も競技順を決める抽選を行います。'}]
+    },
+    {
+      id:'bootstrap-11', name:'モーリーファンタジー浜松市野', address:'静岡県浜松市東区天王町字諏訪1981-3 イオン', prefecture:'静岡県', machineTypes:['おねがいアイプリ','アイプリバース','アイプリステーション'], participation:'参加費1名200円・定員30名を超えた場合抽選', events:[
+        {id:'bootstrap-11-event-1',label:'大会',date:'2026-09-12',dateDisplay:'26年9月12日（土）',ageLimit:'中学生以下',startTime:'10:30',registrationTime:'10:00～10:30',lotteryTime:'10:30',note:'抽選終わり次第大会開催'}]
+    },
+    {
+      id:'bootstrap-12', name:'モーリーファンタジーf 岐阜店', address:'岐阜県岐阜市正木中1丁目2-1 3階 マーサ21 北館 イオン岐阜店', prefecture:'岐阜県', machineTypes:['おねがいアイプリ','アイプリバース','アイプリステーション'], participation:'参加費1名200円・定員30名を超えた場合抽選', events:[
+        {id:'bootstrap-12-event-1',label:'大会①',date:'2026-09-19',dateDisplay:'26年9月19日（土）',ageLimit:'中学生以下',startTime:'11:30',registrationTime:'10:00～11:00',lotteryTime:'11:00',note:'抽選が終了次第大会開始'},
+        {id:'bootstrap-12-event-2',label:'大会②',date:'2026-09-20',dateDisplay:'26年9月20日（日）',ageLimit:'年齢無制限',startTime:'11:30',registrationTime:'10:00～11:00',lotteryTime:'11:00',note:'抽選が終了次第大会開始'}]
+    }
+  ]
 };
